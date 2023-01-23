a08ecd85e22393a075fe071f3adc4d6a14723656
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-23 15:02:48 -0800

    /books route displays all books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import BookList from './components/book/BookList';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import BookList from './components/book/BookList';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path="/books" component={BookList} />
+      </Switch>
     </main>
   );
 }
